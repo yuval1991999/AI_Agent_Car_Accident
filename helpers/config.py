f81021be9c53a145a2d4b484b00d700143b390b1
Pho3nix-knight <idanadar0802@gmail.com>
2025-07-28 11:24:07 +0300

    Update config.py

--- helpers/config.py
+++ helpers/config.py
@@ -10,6 +10,7 @@ RAGAS_QNA_EVALUATION_PATH = "./results/ragas_qna_evaluation_result.txt"
 RAGAS_SUMMARIZE_EVALUATION_PATH = "./results/ragas_summarize_evaluation_result.txt"
 SUMMARY_RESULT_PATH = "./results/summary_result.txt"
 SUMMARY_GROUND_TRUTH_PATH = "./results/refinement_summary_ground_truth.txt"
+RETRIEVED_DOCS_PATH = "./results/retrieved_chunks.txt"
 TOP_K = 5          # Number of documents to retrieve
 CHUNK_SIZE = 500   # Size of text chunks in characters
 CHUNK_OVERLAP = 100
