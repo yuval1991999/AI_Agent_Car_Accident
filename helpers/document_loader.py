9b7072441b55ca808014eb2b1024778c3c0ddd9a
Pho3nix-knight <idanadar0802@gmail.com>
2025-07-30 10:11:45 +0300

    Update document_loader.py

--- helpers/document_loader.py
+++ helpers/document_loader.py
@@ -7,18 +7,3 @@ docs = loader.load()
 
 splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
 split_docs = splitter.split_documents(docs)
-
-# from zep_python.langchain import ZepLoader
-
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-# loader = ZepLoader(
-#     api_url="https://api.getzep.com",
-#     project_id="4953206b-f356-47ab-beb8-6de4a52243a4",
-#     api_key="your_zep_api_key"
-# )
-
-# docs = loader.load()
-
-# splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-# split_docs = splitter.split_documents(docs)
